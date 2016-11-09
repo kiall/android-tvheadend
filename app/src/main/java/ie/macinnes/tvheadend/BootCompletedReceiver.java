85db09636ca7ea047a32bd7df54ba0fa142e66d8
Kiall Mac Innes <kiall@macinnes.ie>
2016-11-09 12:07:06 +0000

    Log intents received by BootCompletedReceiver

--- app/src/main/java/ie/macinnes/tvheadend/BootCompletedReceiver.java
+++ app/src/main/java/ie/macinnes/tvheadend/BootCompletedReceiver.java
@@ -3,13 +3,19 @@ package ie.macinnes.tvheadend;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
+import ie.macinnes.htsp.tasks.GetFileTask;
 import ie.macinnes.tvheadend.sync.EpgSyncService;
 import ie.macinnes.tvheadend.tvinput.TvInputService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = BootCompletedReceiver.class.getName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received intent: " + intent.getAction());
+
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             context.startService(new Intent(context, TvInputService.class));
             context.startService(new Intent(context, EpgSyncService.class));
